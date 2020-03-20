393728e1d0023f14ad4bbea0f54b828b8abbe0f0
Bibek Dahal <bibekdahal.bd16@gmail.com>
2020-03-20 14:42:17 -0500

    Create setup.py to install as pip module

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="hls_thumbnails",
+    version="0.1",
+    packages=['hls_thumbnails'],
+    install_requires=[
+        'boto3',
+        'numpy',
+        'rasterio',
+        'Pillow',
+        'pyhdf'
+    ],
+    extras_require={
+        "develop": ["flake8"],
+        "test": ["pytest", "moto"]
+    },
+    entry_points={
+        'console_scripts': [
+            'run-browse-imagery=hls_thumbnails.run_browse_imagery:run_browse_imagery',  # noqa
+        ],
+    },
+)
