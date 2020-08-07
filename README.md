45f2547f07babd09b3964f8a79128bf1b03ebcf9
Sean Harkins <sharkinsgis@gmail.com>
2020-08-06 22:07:07 -0500

    Merge pull request #3 from NASA-IMPACT/fix/bandnames
    Update testing and refactor to use updated L30 internal band names.

--- README.md
+++ README.md
@@ -1,16 +1,22 @@
 # hls-thumbnails
-
 Generate thumbnails for HLS.
 
-## Installation
 
-Make sure you have python development libraries and hdf4 binaries installed
-in your system.
+### Requirements
+The use of rasterio for HDF does not allow for the regular pip install of rasterio using wheels. It requires a preinstalled gdal version that supports HDF4 installed on the system and install rasterio using
+```
+pip install rasterio --no-binary rasterio
+```
 
-You can then install this application with `pip`:
-
+Installation requires python development libraries and hdf4 binaries. On an Ubuntu/Debian system they can be installed with the following.
 ```bash
-pip install .
+sudo apt-get install build-essential python3-dev python-dev libhdf4-dev # For Python 3
+
+```
+### Installation
+Install for local testing
+```bash
+pip install -e .["test"]
 ```
 
 This will install the `create_thumbnail` executable on the path,
@@ -27,8 +33,8 @@ thumbnail.py -i HLS.L30.T04VER.hdf -o HLS.L30.T04VER.jpeg -s L30
 thumbnail.py -i HLS.S30.T04VER.hdf -o HLS.L30.T04VER.jpeg -s S30
 ```
 
-## Testing
-
+### Tests
+Run Tests on Docker
 ```bash
-pytest
+docker build -t hls_thumbnails . && docker run hls_thumbnails
 ```
--- hls_thumbnails/create_thumbnail.py
+++ hls_thumbnails/create_thumbnail.py
@@ -9,18 +9,17 @@ from pyhdf.SD import SD, SDC
 
 
 # Calculation configurations
-HIGH_THRES = 7500
-HIGH_VAL = 255.
+LOW_VAL = 1
+HIGH_VAL = 255
 
-LOW_VAL = math.e
 LOW_THRES = 100
-
+HIGH_THRES = 7500
 # Instrument based configurations
-SENTINEL_ID = 'S30'
-LANDSAT_ID = 'L30'
+SENTINEL_ID = "S30"
+LANDSAT_ID = "L30"
 BANDS = {
-    SENTINEL_ID: ['B04', 'B03', 'B02'],
-    LANDSAT_ID: ['band04', 'band03', 'band02'],
+    SENTINEL_ID: ["B04", "B03", "B02"],
+    LANDSAT_ID: ["B04", "B03", "B02"],
 }
 
 # Image configurations
@@ -28,8 +27,7 @@ IMG_SIZE = 1000
 
 
 class Thumbnail:
-    def __init__(self, input_file, output_file,
-                 instrument=None, stretch='log'):
+    def __init__(self, input_file, output_file, instrument=None, stretch="log"):
         self.input_file = input_file
         self.output_file = output_file
         self.instrument = instrument
@@ -46,10 +44,8 @@ class Thumbnail:
         """
         self.high_thres = HIGH_THRES
         self.low_thres = LOW_THRES
-        self.low_value = LOW_VAL
-        self.high_thres = math.log(self.high_thres)
-        self.low_thres = math.log(self.low_thres)
-        self.low_value = LOW_VAL
+        self.high_thres = np.log(self.high_thres)
+        self.low_thres = np.log(self.low_thres)
         self.diff = self.high_thres - self.low_thres
 
     def select_constellation(self):
@@ -77,20 +73,26 @@ class Thumbnail:
             extracted_data.append(band_data.get())
         self.attributes = data_file.attributes()
         data_file.end()
-        extracted_data = np.array(extracted_data)
-        extracted_data[np.where(extracted_data <= self.low_thres)] \
-            = self.low_value
-        extracted_data = np.log(extracted_data)
-        extracted_data[np.where(extracted_data >= self.high_thres)] = HIGH_VAL
+        extracted_data = np.ma.masked_less_equal(np.array(extracted_data),-9999)
+        extracted_data = np.ma.log(extracted_data)
+        extracted_data = np.ma.masked_less_equal(extracted_data,self.low_thres).filled(fill_value=self.low_thres)
+        extracted_data = np.ma.masked_greater_equal(extracted_data,self.high_thres).filled(fill_value=self.high_thres)
+        '''
         indices = np.where(
-            (extracted_data > self.low_thres)
-            & (extracted_data < self.high_thres)
+            (extracted_data > self.low_thres) & (extracted_data < self.high_thres)
         )
         extracted_data[indices] = (
-            HIGH_VAL * (extracted_data[indices] - self.low_thres) / self.diff
+                HIGH_VAL * (extracted_data[indices] - self.low_thres) / self.diff
         )
+        '''
+        extracted_data = (
+                HIGH_VAL * (extracted_data - self.low_thres) / self.diff
+        )
+        print(np.max(extracted_data),np.min(extracted_data))
+        extracted_data[np.where(extracted_data <= LOW_VAL)] = LOW_VAL
+        extracted_data[np.where(extracted_data >= HIGH_VAL)] = HIGH_VAL
         extracted_data = extracted_data.astype(rasterio.uint8)
-        file_name = self.input_file.split('/')[-1]
+        file_name = self.input_file.split("/")[-1]
         self.prepare_thumbnail(extracted_data, file_name)
 
     def prepare_thumbnail(self, extracted_data, file_name):
@@ -98,7 +100,7 @@ class Thumbnail:
         Public:
             Creates thumbnail of the granule
         Args:
-            extracted_date - numpy array of the image
+            extracted_data - numpy array of the image
             file_name - Name of the file being opened
         """
         extracted_data = np.rollaxis(extracted_data, 0, 3)
@@ -107,9 +109,9 @@ class Thumbnail:
         img.save(self.output_file)
 
 
-SHORT_OPTIONS = 'i:o:s:'
-LONG_OPTIONS = ['inputfile=', 'outputfile=', 'instrument=']
-HELP_TEXT = 'create_thumbnail -i <input_file> -o <output_file> -s [L30|S30]'
+SHORT_OPTIONS = "i:o:s:"
+LONG_OPTIONS = ["inputfile=", "outputfile=", "instrument="]
+HELP_TEXT = "create_thumbnail -i <input_file> -o <output_file> -s [L30|S30]"
 
 
 def create_thumbnail():
@@ -122,11 +124,11 @@ def create_thumbnail():
 
     input_file, output_file, instrument = None, None, None
     for opt, arg in opts:
-        if opt in ('-i', '--inputfile'):
+        if opt in ("-i", "--inputfile"):
             input_file = arg
-        elif opt in ('-o', '--outputfile'):
+        elif opt in ("-o", "--outputfile"):
             output_file = arg
-        elif opt in ('-s', '--instrument'):
+        elif opt in ("-s", "--instrument"):
             instrument = arg
 
     if input_file is None or output_file is None:
@@ -134,7 +136,7 @@ def create_thumbnail():
         sys.exit(2)
 
     if instrument not in [LANDSAT_ID, SENTINEL_ID]:
-        print('Invalid instrument: ' + instrument)
+        print("Invalid instrument: " + instrument)
         sys.exit(2)
 
     Thumbnail(input_file, output_file, instrument).prepare()
